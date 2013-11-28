10440626f2b152fe164860838c9044d0e2375bf8
Steve <me@slacy.me>
2013-11-27 21:45:42 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var keyboard = require('msi-keyboard');
+
+keyboard.color('left',{color: 'blue', intensity: 'high'});
+keyboard.color('middle',{color: 'green', intensity: 'high'});
+keyboard.color('right',{color: 'red', intensity: 'high'});
+
+
+
+// Use default Hardware enabled mode:
+//keyboard.mode('wave'); // wave, breathe, gaming
